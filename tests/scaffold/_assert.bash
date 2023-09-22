306ebfe75c76993fe8438c5c4b4e5da72ab09161
Alex Skrypnyk <alex@drevops.com>
2023-09-22 10:47:02 +1000

    Simplified php linting.

--- tests/scaffold/_assert.bash
+++ tests/scaffold/_assert.bash
@@ -145,13 +145,10 @@ assert_files_present_php_script() {
   assert_file_contains ".github/workflows/release.yml" "force-crystal"
 
   assert_file_contains "phpcs.xml" "force-crystal.php"
-  assert_file_contains "phpstan.neon" "force-crystal.php"
+  assert_file_contains "phpstan.neon" "force-crystal"
   assert_file_contains "phpunit.xml" "force-crystal"
 
-  assert_file_contains "composer.json" '"cp force-crystal force-crystal.php",'
-  assert_file_contains "composer.json" '"rm force-crystal.php"'
-  assert_file_contains "composer.json" '"phpstan",'
-  assert_file_contains "composer.json" '"phpstan",'
+  assert_file_contains "composer.json" '"cp force-crystal force-crystal.php && phpcs && rm force-crystal.php",'
   assert_file_contains "composer.json" '"force-crystal"'
 
   assert_dir_not_contains_string "${dir}" "php-script"
@@ -173,12 +170,10 @@ assert_files_absent_php_script() {
   assert_file_not_contains ".github/workflows/release.yml" "php-script"
 
   assert_file_not_contains "phpcs.xml" "force-crystal.php"
-  assert_file_not_contains "phpstan.neon" "force-crystal.php"
+  assert_file_not_contains "phpstan.neon" "force-crystal"
   assert_file_not_contains "phpunit.xml" "force-crystal"
 
-  assert_file_not_contains "composer.json" '"cp force-crystal force-crystal.php",'
-  assert_file_not_contains "composer.json" '"rm force-crystal.php"'
-  assert_file_not_contains "composer.json" '"phpstan",'
+  assert_file_not_contains "composer.json" '"cp force-crystal force-crystal.php && phpcs && rm force-crystal.php",'
   assert_file_not_contains "composer.json" '"force-crystal",'
 
   assert_dir_not_contains_string "${dir}" "php-script"
