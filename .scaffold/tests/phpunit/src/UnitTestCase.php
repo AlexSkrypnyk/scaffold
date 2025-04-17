3db5d4a8d4c52be3a9309c64aef9ef05fa3c4658
Alex Skrypnyk <alex@drevops.com>
2025-04-18 07:34:14 +1000

    Fixed test updates logic to correct handle baseline updates.

--- .scaffold/tests/phpunit/src/UnitTestCase.php
+++ .scaffold/tests/phpunit/src/UnitTestCase.php
@@ -42,29 +42,37 @@ abstract class UnitTestCase extends UpstreamUnitTestCase {
    * {@inheritdoc}
    */
   protected function tearDown(): void {
-    if (empty(self::$fixtures)) {
+    if (empty(static::$fixtures)) {
       throw new \RuntimeException('Fixtures directory is not set.');
     }
 
     $is_failure = $this->status() instanceof Failure || $this->status() instanceof Error;
     $has_message = str_contains($this->status()->message(), 'Differences between directories') || str_contains($this->status()->message(), 'Failed to apply patch');
-    $fixture_exists = str_contains(self::$fixtures, DIRECTORY_SEPARATOR . 'init' . DIRECTORY_SEPARATOR);
+    $fixture_exists = str_contains(static::$fixtures, DIRECTORY_SEPARATOR . 'init' . DIRECTORY_SEPARATOR);
     $update_requested = getenv('UPDATE_FIXTURES');
 
     if ($is_failure && $has_message && $fixture_exists && $update_requested) {
-      $baseline = File::dir(static::$fixtures . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . self::BASELINE_DIR);
-      if (str_contains(self::$fixtures, 'baseline')) {
-        File::copyIfExists($baseline . DIRECTORY_SEPARATOR . Index::IGNORECONTENT, self::$sut . DIRECTORY_SEPARATOR . Index::IGNORECONTENT);
-        File::copyIfExists($baseline . DIRECTORY_SEPARATOR . Index::IGNORECONTENT, self::$tmp . DIRECTORY_SEPARATOR . Index::IGNORECONTENT);
+      $baseline = File::dir(static::$fixtures . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . static::BASELINE_DIR);
+
+      $ic_baseline = $baseline . DIRECTORY_SEPARATOR . Index::IGNORECONTENT;
+      $ic_sut = static::$sut . DIRECTORY_SEPARATOR . Index::IGNORECONTENT;
+      $ic_tmp = static::$tmp . DIRECTORY_SEPARATOR . Index::IGNORECONTENT;
+      $ic_fixtures = static::$fixtures . DIRECTORY_SEPARATOR . Index::IGNORECONTENT;
+
+      if (str_contains(static::$fixtures, DIRECTORY_SEPARATOR . static::BASELINE_DIR . DIRECTORY_SEPARATOR)) {
+        File::copyIfExists($ic_baseline, $ic_sut);
+        File::copyIfExists($ic_baseline, $ic_tmp);
         File::rmdir($baseline);
-        File::sync(self::$sut, $baseline);
+        File::sync(static::$sut, $baseline);
         static::replaceVersions($baseline);
-        File::copyIfExists(static::$tmp . DIRECTORY_SEPARATOR . Index::IGNORECONTENT, $baseline . DIRECTORY_SEPARATOR . Index::IGNORECONTENT);
+        File::copyIfExists($ic_tmp, $ic_baseline);
       }
-      File::copyIfExists(self::$fixtures . DIRECTORY_SEPARATOR . Index::IGNORECONTENT, self::$tmp . DIRECTORY_SEPARATOR . Index::IGNORECONTENT);
-      File::rmdir(self::$fixtures);
-      File::diff($baseline, self::$sut, self::$fixtures);
-      File::copyIfExists(self::$tmp . DIRECTORY_SEPARATOR . Index::IGNORECONTENT, self::$fixtures . DIRECTORY_SEPARATOR . Index::IGNORECONTENT);
+      else {
+        File::copyIfExists($ic_fixtures, $ic_tmp);
+        File::rmdir(static::$fixtures);
+        File::diff($baseline, static::$sut, static::$fixtures);
+        File::copyIfExists($ic_tmp, $ic_fixtures);
+      }
     }
 
     parent::tearDown();
