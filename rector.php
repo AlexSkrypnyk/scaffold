0291a986fccb010df8fe169727883863b09f0cd7
Alex Skrypnyk <alex@drevops.com>
2025-12-11 11:19:21 +1100

    Updated Rector configs.

--- rector.php
+++ rector.php
@@ -16,19 +16,13 @@ use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
 use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
 use Rector\CodingStyle\Rector\ClassMethod\NewlineBeforeNewAssignSetRector;
 use Rector\CodingStyle\Rector\FuncCall\CountArrayToEmptyArrayComparisonRector;
-use Rector\CodingStyle\Rector\FunctionLike\FunctionLikeToFirstClassCallableRector;
 use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
 use Rector\Naming\Rector\Assign\RenameVariableToMatchMethodCallReturnTypeRector;
-use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
 use Rector\Naming\Rector\ClassMethod\RenameVariableToMatchNewTypeRector;
 use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
-use Rector\Php81\Rector\Array_\FirstClassCallableRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Privatization\Rector\ClassMethod\PrivatizeFinalClassMethodRector;
-use Rector\Privatization\Rector\MethodCall\PrivatizeLocalGetterToPropertyRector;
-use Rector\Privatization\Rector\Property\PrivatizeFinalClassPropertyRector;
 use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
 use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
@@ -38,10 +32,11 @@ return RectorConfig::configure()
   ])
   ->withPhpSets(php82: TRUE)
   ->withPreparedSets(
-    deadCode: TRUE,
-    codeQuality: TRUE,
-    codingStyle: TRUE,
-    typeDeclarations: TRUE,
+    deadCode: true,
+    codeQuality: true,
+    codingStyle: true,
+    typeDeclarations: true,
+    naming: true,
   )
   ->withSets([
     PHPUnitSetList::PHPUNIT_110,
@@ -56,14 +51,12 @@ return RectorConfig::configure()
     CompleteDynamicPropertiesRector::class,
     CountArrayToEmptyArrayComparisonRector::class,
     DisallowedEmptyRuleFixerRector::class,
-    FirstClassCallableRector::class,
-    FunctionLikeToFirstClassCallableRector::class,
     InlineArrayReturnAssignRector::class,
     NewlineAfterStatementRector::class,
     NewlineBeforeNewAssignSetRector::class,
-
     RemoveAlwaysTrueIfConditionRector::class,
-
+    RenameVariableToMatchMethodCallReturnTypeRector::class,
+    RenameVariableToMatchNewTypeRector::class,
     SimplifyEmptyCheckOnEmptyArrayRector::class,
     // Dependencies.
     '*/vendor/*',
